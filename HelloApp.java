9fb32dd88234d31dbe8b6fefdc745ad66f8bd48d
anvesha0076 <rawatanvesha07@gmail.com>
2026-03-10 13:38:52 +0530

    First Commit

---
+++ HelloApp.java
@@ -0,0 +1,10 @@
+class HelloApp{
+	public static void main (String[] args){
+		
+		//variables and datatypes
+		int num = 100;
+				
+		System.out.println(num);		
+		System.out.println("Hello All...");
+	}
+}
